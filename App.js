1eb813323db251cf128f2d1375244fffff2d4fda
lash1108 <luis.adrian.1104@gmail.com>
2024-05-31 19:16:00 -0600

    xd

--- App.js
+++ App.js
@@ -1,53 +1,46 @@
-import { Text } from "react-native";
-import { PaperProvider } from "react-native-paper";
-import React, { useEffect, useMemo, useState } from "react";
+import { Text } from 'react-native';
+import { PaperProvider } from 'react-native-paper';
+import React, { useEffect, useMemo, useState } from 'react';
 
-import Auth from "./src/screens/Auth";
+import Auth from './src/screens/Auth';
 import AuthContext from "./src/components/context/AuthContext";
-import { getData, setTokenApi } from "./src/api/token";
+import { setTokenApi } from "./src/api/token";
 import Dashboard from "./src/components/Home/DashBoard";
 
 export default function App() {
-  const [auth, setAuth] = useState(undefined);
+    const [auth, setAuth] = useState(undefined);
 
-  useEffect(() => {
-    async function fetchData() {
-      const token = await getData();
-      if (token) {
-        console.log(token, "Ya estoy loggeado");
-      } else {
+    useEffect(() => {
+        console.log("useEffect - setting auth to null");
         setAuth(null);
-      }
+    }, []);
+
+    const login = (user) => {
+        console.log('LOGGING IN');
+        console.log(user);
+        setTokenApi(user.jwt);
+        setAuth({
+            token: user.jwt,
+            idUser: user.id
+        });
     }
-    fetchData();
-  }, []);
 
-  const login = (user) => {
-    console.log("LOGGING IN");
-    console.log(user);
-    setTokenApi(user.jwt);
-    setAuth({
-      token: user.jwt,
-      idUser: user.id,
-    });
-  };
+    const authData = useMemo(() => ({
+        auth,
+        login,
+        logout: () => null
+    }), [auth]);
 
-  const authData = useMemo(
-    () => ({
-      auth,
-      login,
-      logout: () => null,
-    }),
-    [auth]
-  );
+    if (auth === undefined) return null;
 
-  if (auth === undefined) return null;
+    console.log("Rendering App with auth:", auth);
 
-  console.log("Rendering App with auth:", auth);
-
-  return (
-    <AuthContext.Provider value={authData}>
-      <PaperProvider>{auth ? <Dashboard /> : <Auth />}</PaperProvider>
-    </AuthContext.Provider>
-  );
+    return (
+        <AuthContext.Provider value={authData}>
+            <PaperProvider>
+                {auth ?
+                    <Dashboard /> : <Auth />}
+            </PaperProvider>
+        </AuthContext.Provider>
+    );
 }
